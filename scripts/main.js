fec48582c96360202550fbf4165cae3accda9623
Logan Campbell-McPherson <colin@logaan.net>
2012-11-14 20:25:33 +1100

    added knockout bindings for listing thing

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
-jQuery(function() {
-  console.log("Hello, world.");
+function thing(name, parents, children) {
+  var parents  = parents  || [];
+  var children = children || [];
+
+  return {
+    name:     ko.observable(name),
+    parents:  ko.observableArray(parents),
+    children: ko.observableArray(children)
+  };
+};
+
+jQuery(function() {  
+  ko.applyBindings(
+    thing("Shopping", [], [
+      thing("Steak"),
+      thing("Potatoes", [
+        thing("Vegetable")
+      ], [
+        thing("Foo")
+      ]),
+      thing("Bread")
+    ])
+  );
 });
+
